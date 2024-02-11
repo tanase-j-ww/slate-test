027ec7c20425111ab78d6130b48772c4b51b081e
tanase-j-ww <tanase.j@world-wing.com>
2024-02-11 22:53:08 +0900

    create custom bold element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
 "use client";
 
-import { useState } from "react";
-import { createEditor } from "slate";
+import { useCallback, useState } from "react";
+import { Editor, Transforms, createEditor } from "slate";
 import { Editable, Slate, withReact } from "slate-react";
 
 // TypeScript users only add this code
 import { BaseEditor } from "slate";
 import { ReactEditor } from "slate-react";
+export type CustomEditor = BaseEditor & ReactEditor;
 
-type CustomElement = { type: "paragraph"; children: CustomText[] };
-type CustomText = { text: string };
+export type ParagraphElement = {
+  type: "paragraph";
+  children: CustomText[];
+};
+
+export type HeadingElement = {
+  type: "heading";
+  level: number;
+  children: CustomText[];
+};
+
+export type CustomElement = ParagraphElement | HeadingElement;
+
+export type FormattedText = { text: string; bold?: true };
+
+export type CustomText = FormattedText;
 
 declare module "slate" {
-  interface CustomType {
-    Editor: BaseEditor & ReactEditor;
+  interface CustomTypes {
+    Editor: CustomEditor;
     Element: CustomElement;
     Text: CustomText;
   }
@@ -24,30 +39,127 @@ const initialValue = [
     type: "paragraph",
     children: [{ text: "A line of text in a paragraph." }],
   },
-  {
-    type: "headers",
-    children: [{ text: "line" }],
-  },
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
 ];
-
-export default function Home() {
+const Home = () => {
   const [editor] = useState(() => withReact(createEditor()));
+
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
+
   return (
+    // Add a toolbar with buttons that call the same methods.
     <Slate editor={editor} initialValue={initialValue}>
-      <Editable
-        onKeyDown={(event) => {
-          if (event.key === "&") {
-            // Prevent the ampersand character from being inserted.
+      <div>
+        <button
+          onMouseDown={(event) => {
             event.preventDefault();
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText("and");
+            CustomEditor.toggleBoldMark(editor);
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleCodeBlock(editor);
+          }}
+        >
+          Code Block
+        </button>
+      </div>
+      <Editable
+        editor={editor}
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={(event) => {
+          if (!event.ctrlKey) {
+            return;
+          }
+
+          switch (event.key) {
+            case "`": {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock(editor);
+              break;
+            }
+
+            case "b": {
+              event.preventDefault();
+              CustomEditor.toggleBoldMark(editor);
+              break;
+            }
           }
         }}
       />
     </Slate>
   );
-}
+};
+export default Home;
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
+
+// Define a React component to render leaves with bold text.
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    >
+      {props.children}
+    </span>
+  );
+};
+
+// Define our own custom set of helpers.
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const marks = Editor.marks(editor);
+    return marks ? marks.bold === true : false;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    });
+
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    if (isActive) {
+      Editor.removeMark(editor, "bold");
+    } else {
+      Editor.addMark(editor, "bold", true);
+    }
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: (n) => Editor.isBlock(editor, n) }
+    );
+  },
+};
